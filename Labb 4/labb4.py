16e6c3c6291783e9c727ded12e61d61e841791fc
Oliver <you@example.com>
2022-11-07 11:59:43 +0100

    strukturerade koden

--- Labb 4/labb4.py
+++ Labb 4/labb4.py
@@ -6,18 +6,28 @@ class Student:
         self.id_number = id_number
 
     def __str__(self):
-      return str(self.first_name +  self.last_name + self.id_number)
-        
-        
-        
-#main function       
+        return str(self.first_name + self.last_name + self.id_number)
+
+
+def add_student():
+    """Adds new student object"""
+
+
+def edit_student():
+    """Edits an existing student object"""
+
+
+def delete_student():
+    """Deletes a student object"""
+
+
 def main():
-    
+    """Main function of program"""
     full_name = input("Name: ").title().strip()
     first_name, last_name = full_name.split(" ")
     id_number = input("Id number: ")
-    student = Student(first_name, last_name, id_number)
 
     print(student)
+
 
 main()
