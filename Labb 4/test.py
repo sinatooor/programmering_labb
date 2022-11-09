a39c0600881db2b1f4bfa8d69f7dadb28e1513a1
Oliver <oliver.folke@gmail.com>
2022-11-09 13:18:31 +0100

    tar bort dubblett mapp

--- Labb 4/test.py
+++
@@ -1,9 +0,0 @@
-
-
-def main():
-    file = open('students.txt', 'r')
-
-
-
-
-main()
--- labb_4_filer/test.py
+++ labb_4_filer/test.py
@@ -1,21 +1,36 @@
+class School:
+    # School is an object meant to store Student objects
+    def __init__(self, student):
+        print("hello")
+    # Search method to find student based on id_number
+    def search_for_student(self, id_number):
+        searched_id_number = input("What is the id number of the student? ")
+
+class Student:
+    # Student is a object with 3 parameters
+    def __init__(self, first_name, last_name, id_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.id_number = id_number
+
+    def __str__(self):
+        return f"Name: {self.first_name} {self.last_name} ID: {self.id_number}"
+
+
+    @classmethod
+    def get(cls):
+        first_name = input("What is the students fist name? ")
+        last_name = input("What is the students last name? ")
+        id_number = input("What is the students id number? ")
+        return cls(first_name , last_name , id_number)      #will instantiate a object in this classs (the same as Student())
+
+
 
 
 def main():
-    friends = []
-    file = open('students.txt', 'r')
-    for line in file:
-        friends.append(line)
-    
-    i = 1
-    count = 0
-    while i == 1:
-        print(f"Namn: {friends[count+2]}, {friends[count+1]} Personnr: {friends[count]}")
-        if friends[count] == "":
-            break
-        count = count+3
+    a_student = Student(first_name, last_name, id_number)
 
-    file.close()
-
+    print(a_student)
 
 
 
