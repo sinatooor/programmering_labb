92ea5b244f7c7cc2bfe53f4df4f79fcf2d6c33ec
Oliver <oliver.folke@gmail.com>
2022-11-09 14:46:32 +0100

    fixed test

--- labb_6_filer/test.py
+++ labb_6_filer/test.py
@@ -4,7 +4,7 @@ class School:
 
 class Student:
     # student is a object with 3 parameters
-    def __init__(self, first_name, last_name, id_number):
+    def __init__(self, id_number, last_name, first_name):
         self.first_name = first_name
         self.last_name = last_name
         self.id_number = id_number
@@ -18,17 +18,16 @@ class Student:
 def main():
     """Main function of program"""
 
-    id_number = 2
     file = open("students.txt", "r")
     students = []
     amount_of_students = 0
-    while id_number != "":
-        id_number = file.readline().strip()
-        last_name = file.readline().strip()
-        first_name = file.readline().strip()
-        student = Student(first_name,last_name, id_number)
+    while True:
+        student = Student(file.readline().strip(),file.readline().strip(),file.readline().strip())
         students.append(student)
         amount_of_students +=1
+        if file.readline() == "":
+            break
+
     file.close()   
 
 
